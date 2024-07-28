0aabf978f6a755b3c859fbb876ed9e279d98d88b
Bittu, Gowtham (Contractor) <Gowtham_Bittu@comcast.com>
2024-07-28 17:30:26 -0400

    final backend api changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_HOST = 'https://3.90.138.55'; // Replace with your actual API host
+const API_HOST = 'https://backend.smartworkmanagement.com'; // Replace with your actual API host
 
 export default API_HOST;
