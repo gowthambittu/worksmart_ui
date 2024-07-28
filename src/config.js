382dba1bb40cd13e0307f81b75e861e72230f9f7
Bittu, Gowtham (Contractor) <Gowtham_Bittu@comcast.com>
2024-07-28 15:12:55 -0400

    api changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_HOST = 'http://localhost:8080'; // Replace with your actual API host
+const API_HOST = 'http://3.90.138.55'; // Replace with your actual API host
 
 export default API_HOST;
