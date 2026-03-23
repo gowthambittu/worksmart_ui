94642d8cd244e9596d6a1ea5ac5167403a473d51
Gowtham Bittu <gowthambittu@Gowthams-MacBook-Air.local>
2026-03-23 22:40:57 +0530

    chore: set production API host for render deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-// config.js
-const API_HOST = (process.env.REACT_APP_API_HOST || 'http://localhost:8080').replace(/\/$/, '');
-
+const API_HOST = process.env.REACT_APP_API_HOST || 'https://worksmart-backend-insi.onrender.com';
 export default API_HOST;
